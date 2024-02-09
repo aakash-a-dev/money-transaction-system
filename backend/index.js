c35de536baea824f9b00172926fcd9e3d60aa217
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-10 01:33:58 +0530

    Completed DB Connection

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,55 @@
 const express = require("express");
 const cors = require("cors");
 const rootRouter = require("./routes/index")
-
+const {signupBody} = require("./types");
+const router = require("./routes/user");
 const app = express();
+const { User } = require("./db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./config");
 
+require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1", rootRouter);
 
-app.get("/", (req, res) => {
-    
-})
+app.post("/signup", async (req, res) => {
+    console.log(process.env.JWT_SECRET);
+    const { success } = signupBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect Input"
+        })
+    }
 
-app.post("/signup", (req, res) => {
-    
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect Inputs"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
 })
 
 app.listen(3000, () => {
