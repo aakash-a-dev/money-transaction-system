c6d2b9ea0f75af77f30a6fef2c5a37f502619d78
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-11 12:01:37 +0530

    Transaction route done

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("./config");
 const bcrypt = require('bcrypt');
 const { authMiddleware } = require("./middleware");
+const { default: mongoose } = require("mongoose");
 
 require('dotenv').config();
 app.use(express.json());
@@ -133,6 +134,39 @@ router.post("/signin", async (req, res) => {
         message: "Error while logging in"
     })
 
+})
+
+router.post("/transfer", authMiddleware, async (req, res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    const account = await Account.findOne({ userId: req.userId }).session(session);
+
+    if (!account || account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message:"Insufficient Balance"
+        })
+    }
+
+    const toAccount = await Account.findOne({ userId: to }).session(session);
+
+    if (!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid Account"
+        });
+    }
+
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer Successful"
+    })
 })
 
 
