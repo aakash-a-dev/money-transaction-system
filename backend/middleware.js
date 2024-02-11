b5b1f03cf8a89b88b23ac63d779b3c599e89a9d3
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-11 12:51:15 +0530

    Middleware Correction done

--- backend/middleware.js
+++ backend/middleware.js
@@ -2,9 +2,9 @@ const { JWT_SECRET } = require("./config");
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-    const authHeader = req.header.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!authHeader || authHeader.startsWith('Bearer ')) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(403).json({});
     }
 
