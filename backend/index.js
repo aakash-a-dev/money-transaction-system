6cd0dc28220386253ce763971e4819f47c4c17c4
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-11 12:04:40 +0530

    Updated Route file and made the correction

--- backend/index.js
+++ backend/index.js
@@ -44,15 +44,6 @@ router.get("/bulk", async (req, res) => {
     })
 })
 
-router.get("/balance", authMiddleware, async (req, res) => {
-    const account = await Account.findOne({
-        userId: req.userId
-    });
-
-    res.json({
-        balance: account.balance
-    })
-})
 
 router.post("/signup", async (req, res) => {
     const { success } = signupBody.safeParse(req.body)
@@ -134,39 +125,6 @@ router.post("/signin", async (req, res) => {
         message: "Error while logging in"
     })
 
-})
-
-router.post("/transfer", authMiddleware, async (req, res) => {
-    const session = await mongoose.startSession();
-
-    session.startTransaction();
-    const { amount, to } = req.body;
-
-    const account = await Account.findOne({ userId: req.userId }).session(session);
-
-    if (!account || account.balance < amount) {
-        await session.abortTransaction();
-        return res.status(400).json({
-            message:"Insufficient Balance"
-        })
-    }
-
-    const toAccount = await Account.findOne({ userId: to }).session(session);
-
-    if (!toAccount) {
-        await session.abortTransaction();
-        return res.status(400).json({
-            message: "Invalid Account"
-        });
-    }
-
-    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
-
-    await session.commitTransaction();
-    res.json({
-        message: "Transfer Successful"
-    })
 })
 
 
