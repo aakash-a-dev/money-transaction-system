3a4db622d75079614c825beb7b2a91cc9412e429
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-12 13:01:07 +0530

    Updated the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />}/>
           <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/send" element={<Send />}/>
-          
+          <Route path="/send" element={<Send />}/>     
         </Routes>
       </BrowserRouter>
     </>
